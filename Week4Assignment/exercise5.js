9902a390e55070a107e73c3ff1c101c3e7971aa1
Joseph George <josephgeorge7797@gmail.com>
2022-02-28 17:28:37 +0000

    Week 4 Exercise 5

--- Week4Assignment/exercise5.js
+++ Week4Assignment/exercise5.js
@@ -1,3 +1,7 @@
+document.addEventListener('DOMContentLoaded', myLoadFunction);
+var element = document.getElementById('identifier');
+var headingValue = "JavaScript Tutorial Alerts";
+
 function OnHeadingClick() {
     var element = document.getElementById('identifier');
     element.firstChild.nodeValue = 'Heading has been changed';
@@ -6,9 +10,20 @@ function OnHeadingClick() {
     element.firstChild.nodeValue = 'New Lorem Ipsum';
 }
 
+function textChange() {
+    var element = document.getElementById('identifier')
+    element.firstChild.nodeValue = 'Mouse hover made it change';
+}
+
+function textChangeRevert() {
+    var element = document.getElementById('identifier')
+    element.firstChild.nodeValue = headingValue;
+}
+
+
 function myLoadFunction() {
     var element = document.getElementById('identifier')
     element.addEventListener('click', OnHeadingClick);
+    element.addEventListener('mouseenter', textChange);
+    element.addEventListener('mouseleave', textChangeRevert);
 }
-
-document.addEventListener('DOMContentLoaded', myLoadFunction);
