e9efa5abf207777683aca862ea4121a144ec59d6
Joseph George <josephgeorge7797@gmail.com>
2022-02-28 16:01:02 +0000

    Execise 2 Week 4

---
+++ Week4Assignment/exercise1.js
@@ -0,0 +1,8 @@
+function print5() {
+    for (let i = 1; i <= 5; i++) {
+        alert(i);
+    }
+}
+
+print5();
+print5();
---
+++ Week4Assignment/exercise2.js
@@ -0,0 +1,8 @@
+function print5() {
+    for (let i = 1; i <= 5; i++) {
+        console.log(i);
+    }
+}
+
+print5();
+print5();
